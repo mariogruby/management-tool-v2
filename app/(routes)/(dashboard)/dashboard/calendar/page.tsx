b129057cd0a33528d3efd708db2107fec6bd57cc
mario ruby <120481899+mariogruby@users.noreply.github.com>
2026-04-12 15:30:58 +0200

    commit vacio modificacion sin valor

--- app/(routes)/(dashboard)/dashboard/calendar/page.tsx
+++ app/(routes)/(dashboard)/dashboard/calendar/page.tsx
@@ -108,7 +108,7 @@ export default function CalendarPage() {
       </div>
 
       <div className="flex gap-6 flex-1 min-h-0">
-        {/* Calendar grid */}
+        {/* Calendar grid  */}
         <div className="flex-1 min-w-0 flex flex-col gap-3">
           {/* Month nav */}
           <div className="flex items-center justify-between">
